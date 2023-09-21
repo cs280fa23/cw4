852f749e6bd86bd403c639749ee65ec2d52f5d99
Ali Madooei <alimadooei@gmail.com>
2023-09-21 08:32:36 -0400

    Initial Commit

---
+++ example01.js
@@ -0,0 +1,18 @@
+function getJhed(student) {
+
+}
+
+function getCourses(jhed) {
+
+}
+
+function getGrades(jhed, courses) {
+
+}
+
+console.log("Listening to events!");
+const jhed = getJhed("John Doe");
+const courses = getCourses(jhed);
+const grades = getGrades(jhed, courses);
+console.log(grades);
+console.log("Still listening to events!");
